64f0b18c21e3a2eadbeec28331767cd844e7e26d
Erik Doss <edoss@umich.edu>
2022-12-16 12:03:34 -0500

    commit 4

--- ec2.md
+++ ec2.md
@@ -1,3 +1,4 @@
 ## Paste your link below
 https://trello.com/invite/b/yrtbnfjN/ATTI6cc386d7e67fa31ba2d8ea3a76ab2d97BC82518B/duelassesment
 
+http://54.83.190.138/
--- server.js
+++ server.js
@@ -15,10 +15,7 @@ app.use(express.static(`${__dirname}/public`))
 
 
 
-app.get('/',(req,res) => {
-    res.status(200).sendFile(path.join(__dirname,'/public/index.html'))
 
-})
 
 app.get('/api/robots', (req, res) => {
     try {
