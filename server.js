7480af212d79a4c0830e150ac881dda6e6b4f14f
Erik Doss <edoss@umich.edu>
2022-12-16 14:37:27 -0500

    commit 5

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use(express.static(`${__dirname}/public`))
 
 
 
+app.get('/',(req,res) => {
+    res.status(200).sendFile(path.join(__dirname,'public/index.html'))
 
+})
 
 app.get('/api/robots', (req, res) => {
     try {
